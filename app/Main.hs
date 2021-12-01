f02ef45d5719e5d34435ceb298fc775fbdf68014
Simeon Kaul <simeon.kaul@devboost.com>
2021-12-01 21:11:39 +0100

    [01] Formatting

--- app/Main.hs
+++ app/Main.hs
@@ -4,7 +4,7 @@ import Lib
 
 main :: IO ()
 main = do
-    input <- readFile "input/input.01"
-    putStrLn $ "Level 1a: " ++ show (lvl1a input)
+  input <- readFile "input/input.01"
+  putStrLn $ "Level 1a: " ++ show (lvl1a input)
 
-    putStrLn $ "Level 1b: " ++ show (lvl1b input)
+  putStrLn $ "Level 1b: " ++ show (lvl1b input)
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,16 +1,17 @@
 module Lib
-    ( lvl1a
-    , lvl1b
-    ) where
+  ( lvl1a,
+    lvl1b,
+  )
+where
 
 countIncreases :: [Integer] -> Int
-countIncreases xs = length $ filter (>0) $ zipWith (-) (tail xs) xs
+countIncreases xs = length $ filter (> 0) $ zipWith (-) (tail xs) xs
 
 lvl1a :: String -> Int
-lvl1a s = countIncreases $ map read $ lines s
+lvl1a = countIncreases . map read . lines
 
 lvl1b :: String -> Int
 lvl1b s = countIncreases w3
-    where
-        xs = (map read $ lines s) :: [Integer]
-        w3 = zipWith3 (\a b c -> a + b + c) xs (tail xs) (tail $ tail xs)
+  where
+    xs = map read $ lines s
+    w3 = zipWith3 (\a b c -> a + b + c) xs (tail xs) (tail $ tail xs)
