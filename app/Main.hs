ed02d943df90bf870e4855ba7532c03ff7763b31
Simeon Kaul <simeon.kaul@devboost.com>
2021-12-03 00:07:53 +0100

    [02] Implement

--- app/Main.hs
+++ app/Main.hs
@@ -3,7 +3,11 @@ module Main where
 import Lib
 
 main :: IO ()
-main = do
-  input <- readFile "input/input.01"
-  putStrLn $ "Level 1a: " ++ show (lvl1a input)
-  putStrLn $ "Level 1b: " ++ show (lvl1b input)
+main
+  -- input1 <- readFile "input/input.01"
+  -- putStrLn $ "Level 1a: " ++ show (lvl1a input1)
+  -- putStrLn $ "Level 1b: " ++ show (lvl1b input1)
+ = do
+  input2 <- readFile "input/input.02"
+  putStrLn $ "Level 2a: " ++ show (level2a input2)
+  putStrLn $ "Level 2b: " ++ show (level2b input2)
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,8 +1,17 @@
 module Lib
   ( lvl1a
   , lvl1b
+  , level2a
+  , level2b
+  , Diving(..)
+  , Submarine(..)
+  , steer
+  , prod
   ) where
 
+import Control.Monad (liftM2)
+
+-- Level 1
 countIncreases :: (Ord a, Num a) => [a] -> Int
 -- countIncreases xs = length $ filter (> 0) $ zipWith (-) (tail xs) xs
 countIncreases = length . filter (< 0) . (zipWith (-) <*> tail)
@@ -11,7 +20,50 @@ lvl1a :: String -> Int
 lvl1a = countIncreases . map read . lines
 
 window3 :: Num d => [d] -> [d]
+-- window3 xs = zipWith3 (\a b c -> a + b + c) xs (tail xs) (tail $ tail xs)
 window3 = (zipWith3 (\a b c -> a + b + c) =<< tail) =<< tail
 
 lvl1b :: String -> Int
 lvl1b = countIncreases . window3 . map read . lines
+
+-- Level 2
+data Diving
+  = Forward Int
+  | Down Int
+  | Up Int
+  deriving (Show)
+
+doRead ("forward":m:rest) = [(Forward $ read m, unwords rest)]
+doRead ("down":m:rest) = [(Down $ read m, unwords rest)]
+doRead ("up":m:rest) = [(Up $ read m, unwords rest)]
+doRead _ = []
+
+instance Read Diving where
+  readsPrec _ = doRead . words
+
+dive :: (Int, Int) -> Diving -> (Int, Int)
+dive (h, d) (Forward m) = (h + m, d)
+dive (h, d) (Down m) = (h, d + m)
+dive (h, d) (Up m) = (h, d - m)
+
+level2a :: String -> Int
+level2a = uncurry (*) . foldl dive (0, 0) . map read . lines
+
+steer :: Submarine -> Diving -> Submarine
+steer s (Forward x) = s {hPos = x + hPos s, depth = x * aim s + depth s}
+steer s (Down x) = s {aim = x + aim s}
+steer s (Up x) = s {aim = aim s - x}
+
+data Submarine =
+  Submarine
+    { hPos :: Int
+    , depth :: Int
+    , aim :: Int
+    }
+  deriving (Show)
+
+level2b :: String -> Int
+level2b = prod . foldl steer (Submarine 0 0 0) . map read . lines
+
+prod :: Submarine -> Int
+prod = liftM2 (*) hPos depth
