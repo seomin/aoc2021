c854e37c516e4d4d203838d88cc23e0a385aaa9b
Simeon Kaul <simeon.kaul@devboost.com>
2021-12-02 02:33:57 +0100

    [01] Reverse bind...

--- src/Lib.hs
+++ src/Lib.hs
@@ -11,7 +11,7 @@ lvl1a :: String -> Int
 lvl1a = countIncreases . map read . lines
 
 window3 :: Num d => [d] -> [d]
-window3 = (zipWith3 (\a b c -> a + b + c) <*> tail) =<< tail
+window3 = (zipWith3 (\a b c -> a + b + c) =<< tail) =<< tail
 
 lvl1b :: String -> Int
 lvl1b = countIncreases . window3 . map read . lines
