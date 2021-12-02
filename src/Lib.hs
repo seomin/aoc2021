07344accf2afe9bf2bdfe11b32fbe3f24fc5d5cc
Simeon Kaul <simeon.kaul@devboost.com>
2021-12-02 02:21:23 +0100

    [01] Slightly better actually

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,14 +3,15 @@ module Lib
   , lvl1b
   ) where
 
-countIncreases :: [Integer] -> Int
+countIncreases :: (Ord a, Num a) => [a] -> Int
 -- countIncreases xs = length $ filter (> 0) $ zipWith (-) (tail xs) xs
 countIncreases = length . filter (< 0) . (zipWith (-) <*> tail)
 
 lvl1a :: String -> Int
 lvl1a = countIncreases . map read . lines
 
+window3 :: Num d => [d] -> [d]
+window3 = (zipWith3 (\a b c -> a + b + c) <*> tail) =<< tail
+
 lvl1b :: String -> Int
-lvl1b =
-  countIncreases .
-  ((zipWith3 (\a b c -> a + b + c) <*> tail) =<< tail) . map read . lines
+lvl1b = countIncreases . window3 . map read . lines
