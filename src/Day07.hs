30cf7007a650453396c8b5f0a05971c0be68d587
Simeon Kaul <simeon.kaul@devboost.com>
2021-12-08 00:58:19 +0100

    [07.2] Solve

--- src/Day07.hs
+++ src/Day07.hs
@@ -13,8 +13,23 @@ day07 = Day "07" "input/07.txt" solution1 solution2
 solution1 :: Solution
 solution1 [input] = sum $ map (abs . (median -)) positions
   where
-    positions = map read $ splitOn "," input
+    positions = parsePostions input
     median = sort positions !! (length positions `div` 2)
 
+parsePostions :: String -> [Integer]
+parsePostions = map read . splitOn ","
+
 solution2 :: Solution
-solution2 = error "Not implemented"
+solution2 [input] =
+  takeSmallest [sum [distance . abs . (i -) $ p | p <- positions] | i <- [0 ..]]
+  where
+    positions = parsePostions input
+
+takeSmallest :: [Integer] -> Integer
+takeSmallest [a] = a
+takeSmallest (a:b:rest)
+  | a <= b = a
+  | otherwise = takeSmallest (b : rest)
+
+distance :: Integer -> Integer
+distance n = (n * n + n) `div` 2
