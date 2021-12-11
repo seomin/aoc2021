cc03dc8f0774a7a70e18fc59bff088bd84f339e6
Simeon Kaul <simeon.kaul@devboost.com>
2021-12-11 01:33:50 +0100

    [10.2] Solve

--- src/Day10.hs
+++ src/Day10.hs
@@ -2,13 +2,15 @@ module Day10
   ( day10
   ) where
 
+import Data.List (sort)
 import Data.Maybe (mapMaybe)
+import Foreign (callocArray0)
 import Support
 
 day10 = Day "10" "input/10.txt" solution1 solution2
 
 solution1 :: Solution
-solution1 = fromIntegral . sum . map score . mapMaybe (corrupted "")
+solution1 = fromIntegral . sum . map score . mapMaybe (corrupted . parse "")
 
 score :: Char -> Int
 score ')' = 3
@@ -17,17 +19,47 @@ score '}' = 1197
 score '>' = 25137
 score _ = error "unreachable"
 
-corrupted :: String -> String -> Maybe Char
-corrupted stack (c:cs)
-  | c == '(' = corrupted (')' : stack) cs
-  | c == '[' = corrupted (']' : stack) cs
-  | c == '{' = corrupted ('}' : stack) cs
-  | c == '<' = corrupted ('>' : stack) cs
+corrupted :: ParseResult -> Maybe Char
+corrupted (Corrupted c) = Just c
+corrupted _ = Nothing
+
+data ParseResult
+  = Valid
+  | Corrupted Char
+  | Incomplete String
+
+parse :: String -> String -> ParseResult
+parse stack (c:cs)
+  | c == '(' = parse (')' : stack) cs
+  | c == '[' = parse (']' : stack) cs
+  | c == '{' = parse ('}' : stack) cs
+  | c == '<' = parse ('>' : stack) cs
   | otherwise =
     if c == head stack
-      then corrupted (tail stack) cs
-      else Just c
-corrupted _ [] = Nothing -- Incomplete
+      then parse (tail stack) cs
+      else Corrupted c
+parse [] [] = Valid
+parse stack [] = Incomplete stack
+
+incomplete :: ParseResult -> Maybe String
+incomplete (Incomplete s) = Just s
+incomplete _ = Nothing
 
 solution2 :: Solution
-solution2 = error "Not implemented"
+solution2 = median . map scoreCompletion . mapMaybe (incomplete . parse "")
+
+scoreCompletion :: String -> Integer
+scoreCompletion =
+  foldl (\acc i -> 5 * acc + fromIntegral i) 0 . map scoreCompletionChars
+
+scoreCompletionChars :: Char -> Int
+scoreCompletionChars c =
+  case c of
+    ')' -> 1
+    ']' -> 2
+    '}' -> 3
+    '>' -> 4
+    _ -> error "unreachable"
+
+median :: [Integer] -> Integer
+median is = sort is !! (length is `div` 2)
--- test/Day10Spec.hs
+++ test/Day10Spec.hs
@@ -12,7 +12,7 @@ spec = do
     it "can solve the example" $ do
       exampleData <- liftIO $ readFile "test/input/10.txt"
       part1 day10 (lines exampleData) `shouldBe` 26397
-  -- describe "part 2" $ do
-  --   it "can solve the example" $ do
-  --     exampleData <- liftIO $ readFile "test/input/10.txt"
-  --     part2 day10 (lines exampleData) `shouldBe` 1134
+  describe "part 2" $ do
+    it "can solve the example" $ do
+      exampleData <- liftIO $ readFile "test/input/10.txt"
+      part2 day10 (lines exampleData) `shouldBe` 288957
